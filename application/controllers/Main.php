92bfc1e1748b976f41119505032b1769c0aedaad
Xmal Morthen <xmal.morthen@gmail.com>
2015-03-24 22:35:35 -0600

    adecuaciones

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -21,6 +21,7 @@ class main extends CI_Controller {
                     if ($this->decryptwhatsapp->do_decrypt($path_to_upload,$data)) {                         
                         $params = array(
                             'data'  => $data,
+                            'path_to_upload' => $path_to_upload,
                             'view'  => $data['whatsapp_xtract']['file_name']
                         );
                         $this->session->set_flashdata('view',$params);
@@ -45,8 +46,8 @@ class main extends CI_Controller {
     public function show_result(){        
         $params = $this->session->flashdata('params');
         $data = $params['data'];
+        $path_to_upload = $params['path_to_upload'];
         $data['whatsapp_xtract']['view'] = $this->load->view(WHATSAPP_XTRACT_OUTPUT_VIEW."/{$params['view']}",NULL,TRUE);
-
         @$this->uploader->remove_dir($path_to_upload, TRUE);
         @unlink($data['whatsapp_xtract']['output_file']. EXT);
         
