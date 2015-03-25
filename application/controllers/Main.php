963037c050999593697f8164d8178a3833816244
Xmal Morthen <xmal.morthen@gmail.com>
2015-03-24 22:31:44 -0600

    adecuaciones

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -2,30 +2,42 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class main extends CI_Controller {
+    public function __construct()
+    {
+        parent::__construct();       
+        $this->load->library("uploader");
+    }
+    
     public function index()
     {
         try
         {
             $data['post'] = FALSE;
             if($this->input->post("submit")){
-                $this->load->library("uploader");
-                
                 $path_to_upload = DATABASE_PATH . '/' . utils::get_uniqueidentifier();
                 $data = $this->uploader->do_upload($path_to_upload);
                 if (is_array($data) && isset($data['success']) && $data['success']){
                     $this->load->library("decryptwhatsapp");
-                    if ($this->decryptwhatsapp->do_decrypt($path_to_upload,$data)) {
-                        $data['whatsapp_xtract']['view'] = $this->load->view(WHATSAPP_XTRACT_OUTPUT_VIEW."/{$data['whatsapp_xtract']['file_name']}",NULL,TRUE);
-                        $this->uploader->remove_dir($path_to_upload, TRUE);                                                
-                        @unlink($data['whatsapp_xtract']['output_file']. EXT);
+                    if ($this->decryptwhatsapp->do_decrypt($path_to_upload,$data)) {                         
+                        $params = array(
+                            'data'  => $data,
+                            'view'  => $data['whatsapp_xtract']['file_name']
+                        );
+                        $this->session->set_flashdata('view',$params);
+                        redirect("main/show_result");
                     }                    
                 }
                 
-                /*$data['whatsapp_xtract']['success'] = TRUE;
-                $data['whatsapp_xtract']['message'] = 'Base de datos procesada correctamente...!!!';
-                $data['whatsapp_xtract']['view'] = $this->load->view(WHATSAPP_XTRACT_OUTPUT_VIEW."/550d730710cf6",NULL,TRUE);*/
-                
+                $data['whatsapp_xtract']['success'] = TRUE;
+                $data['whatsapp_xtract']['message'] = 'Base de datos procesada correctamente...!!!';                
                 $data['post'] = TRUE;
+                $params = array(
+                    'data'  => $data,
+                    'view'  => '550d730710cf6'
+                );
+                $this->session->set_flashdata('params',$params);
+                redirect("main/show_result");
+                
             }
             $this->load->view('main',array('data' => $data));
         }
@@ -34,6 +46,17 @@ class main extends CI_Controller {
             log_message("error",$err->getMessage());
             return show_error($err->getMessage());
         }
+    }
+    
+    public function show_result(){        
+        $params = $this->session->flashdata('params');
+        $data = $params['data'];
+        $data['whatsapp_xtract']['view'] = $this->load->view(WHATSAPP_XTRACT_OUTPUT_VIEW."/{$params['view']}",NULL,TRUE);
+
+        @$this->uploader->remove_dir($path_to_upload, TRUE);
+        @unlink($data['whatsapp_xtract']['output_file']. EXT);
+        
+        $this->load->view('main',array('data' => $data));
     }
     
     public function administracion(){
